252c0baef9d3b724801b82f57c1ac67b59fdb6b2
Ahmed <ahmed@192.168.152.128>
2021-03-29 01:05:25 +0200

    finish leagues screen

--- Sports/LeaguesViewController.swift
+++ Sports/LeaguesViewController.swift
@@ -9,37 +9,73 @@ import UIKit
 import Alamofire
 
 class LeaguesViewController: UIViewController {
-
+    
     @IBOutlet weak var leagueTableView: UITableView!
     var spName:String?
     var leagueList = [League]()
-    let url = "https://www.thesportsdb.com/api/v1/json/1/all_leagues.php"
+    var newLeagueList = [League]()
+    var lastLeagueList = [Detail]()
+    let url = "https://www.thesportsdb.com/api/v1/json/1/"
+    let urlDefaultImage = "https://www.allsportsphysio.com.au/wp-content/uploads/2020/08/Return-to-sport-tile-landscape-300x183.jpg"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         leagueTableView.delegate = self
         leagueTableView.dataSource = self
-
+        
         self.fetchData()
+        //self.getLeagueDetals()
     }
     
     func fetchData(){
-    
-        Alamofire.request(url).responseJSON {(response) in
+        Alamofire.request(url + "all_leagues.php").responseJSON {(response) in
             let result = response.data
             var response:Leagues?
             do{
                 response = try JSONDecoder().decode(Leagues.self,from: result!)
                 self.leagueList = response!.leagues!
-                self.leagueTableView.reloadData()
-
+                for element in self.leagueList{
+                    if (element.strSport == self.spName){
+                        self.newLeagueList.append(element)
+                    }
+                }
+                self.getLeagueDetails()
+                
             }catch{
                 print(error)
             }
         }
     }
     
+    func getLeagueDetails(){
+        for element in self.newLeagueList{
+            let id = Int(element.idLeague!)
+            Alamofire.request(url + "lookupleague.php?id=\(id!)").responseJSON {(response) in
+                let result = response.data
+                var response:LeagueDetails?
+                do{
+                    response = try JSONDecoder().decode(LeagueDetails.self,from: result!)
+                   
+                    self.lastLeagueList.append(response!.leagues![0])
+                    self.leagueTableView.reloadData()
+
+                }catch{
+                    print(error)
+                }
+            }
+        }
+    }
+    
+}
+struct LeagueDetails:Codable{
+    let leagues:[Detail]?
+}
+
+struct Detail:Codable{
+    var strLeague:String?
+    var strYoutube:String?
+    var strBadge:String?
 }
 struct Leagues:Codable{
     let leagues:[League]?
@@ -63,15 +99,18 @@ extension LeaguesViewController: UITableViewDataSource{
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return leagueList.count
+        return lastLeagueList.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! LeagueTableViewCell
-        let league = leagueList[indexPath.row]
+        
+        let league = lastLeagueList[indexPath.row]
         
         cell.leagueName.text = league.strLeague
-        cell.leagueImage.image = UIImage(named:"image")
+        
+        cell.leagueImage.sd_setImage(with: URL(string: league.strBadge ?? urlDefaultImage), placeholderImage:UIImage(named: "image"))
+        
         cell.leagueImage.roundedImage()
         return cell
     }
